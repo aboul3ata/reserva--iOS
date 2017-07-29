f41c6bd94ad07328a5d142196f4a7c17de6b430c
Ali Abouelatta <ata311@nyu.edu>
2017-07-29 17:44:42 +0200

    redesigned login screen
    new pic, created custom sign in with fb button

--- reserva/MainVC.swift
+++ reserva/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  reserva
 //
 //  Created by Ali Abouelatta on 6/2/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
